31ddd7f579d6fbef2d16092baf05fb482938a3f3
Amandacdev <amanda.crz.aj@gmail.com>
2024-08-25 23:52:17 -0300

    Atualizando informações e ajustes de estilo

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,32 @@
 export default function Sidebar(props) {
   return (
+       <div className="sidebar">
+        <img
+          src="https://github.com/Amandacdev.png"
+          alt="Foto de Amanda Cruz"
+        ></img>
+
+        <div className="about">
+          <h3>Olá, sou Amanda!</h3>
+          <h2>Desenvolvedora</h2>
+        </div>
+
+        <a target="_blank" href="https://drive.google.com/file/d/1acVpkPacfo53Kg0rOOvzSUFsUiHXwK78/view">
+            <button className="curriculum-sidebar">Currículo</button>
+          </a>
+          
+          <div className="position-absolute bottom-0">
+          <div  class="align-items-center">
+            <ul className={props.className}>{props.children}</ul>
+          </div>
+          <p> 2024 by Amanda</p>
+        </div>
+    </div>
+
+
+  );
+}
+    /*
     <div className="sidebar">
         <img
           src="https://github.com/Amandacdev.png"
@@ -22,5 +49,4 @@ export default function Sidebar(props) {
           <p> 2024 by Amanda</p>
         </div>
     </div>
-  );
-}
+    */
